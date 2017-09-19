9aacc34fc3b44a6f1aa8d96035421df4c9365968
Spartan-65 <liuyanchong@outlook.com>
2017-09-19 19:59:12 +0800

    update

--- cdoj/2017 UESTC Training for Data Structures/可以说是非常豹笑了/main.cpp
+++ cdoj/2017 UESTC Training for Data Structures/可以说是非常豹笑了/main.cpp
@@ -5,18 +5,20 @@ int Set[maxn<<1];
 void init()
 {
     for(int i=0;i<maxn<<1;i++)
-    {
         Set[i]=i;
-    }
 }
-void meg(int op,int x,int y)
-{
-
-}
-void Find(int x)
+int Find(int x)
 {
     if(Set[x]==x) return x;
     return Find(Set[x]);
+}
+void meg(int x,int y)
+{
+    int fx=Find(x),fy=Find(y);
+    if(fx!=fy)
+    {
+        Set[fx]=fy;
+    }
 }
 int main()
 {
@@ -24,6 +26,24 @@ int main()
     while(~scanf("%d%d",&n,&m))
     {
         init();
+        for(int i=0;i<m;i++)
+        {
+            int x,a,b;scanf("%d%d%d",&x,&a,&b);
+            if(x)
+                meg(a,b),meg(a+maxn,b+maxn);
+            else meg(a,b+maxn),meg(a+maxn,b);
+        }
+        bool flag=1;
+        for(int i=1;i<=n;i++)
+        {
+            if(Find(i)==Find(i+maxn))
+            {
+                flag=0;break;
+            }
+        }
+        if(flag)
+            cout<<"YES"<<endl;
+        else cout<<"NO"<<endl;
     }
     return 0;
 }
