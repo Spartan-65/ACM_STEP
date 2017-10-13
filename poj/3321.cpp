191668552795c6d6635c1e46e431b489d02fc05e
Spartan-65 <liuyanchong@outlook.com>
2017-10-13 22:10:05 +0800

    update

--- poj/3321.cpp
+++ poj/3321.cpp
@@ -4,24 +4,49 @@ const int maxn=1e6+8;
 struct data{
 	int to,next;
 }e[maxn<<1];
-int n,cnt;
-int last[maxn];
+int n,cnt,m,id;
+int last[maxn],q[maxn],st[maxn],ed[maxn];
 void init(){
-	cnt=0;
+	cnt=id=0;
+	memset(last,0,sizeof(last));
+	memset(ed,0,sizeof(ed));
+	memset(st,0,sizeof(st));
 }
-void insert(int u,int v)
+void ins(int u,int v)
 {
 	e[++cnt].to=v;e[cnt].next=last[u];last[u]=cnt;
 	e[++cnt].to=u;e[cnt].next=last[v];last[v]=cnt;
 }
+
+void dfs(int x,int fa)
+{
+    cout<<"begin"<<endl;
+    cout<<x<<' '<<fa<<endl;
+    q[++id]=x;
+    st[x]=id;
+    for(int i=last[x];i;i=e[i].next)
+    {
+        if(e[i].to!=fa)
+        {
+            dfs(e[i].to,x);
+        }
+    }
+    ed[x]=id;
+    cout<<x<<' '<<id<<endl;
+}
+
 int main()
 {
 	scanf("%d",&n);
 	init();
 	for(int i =1;i<n;i++)
 	{
-		int u,v;scanf("%d",&u,&v);
-		insert(u,v);
+		int u,v;scanf("%d%d",&u,&v);
+		ins(u,v);
 	}
+	dfs(1,0);
+	scanf("%d",&m);
+	char ch[10];
+	//build(1,1,n);
 	return 0;
 }
