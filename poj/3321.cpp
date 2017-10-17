0b739a0b2fad563d9e6ba1dd8f1a0960c6651a58
Spartan-65 <liuyanchong@outlook.com>
2017-10-17 15:10:51 +0800

    poj 3321

--- poj/3321.cpp
+++ poj/3321.cpp
@@ -1,4 +1,8 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <cstdio>
+#include <vector>
 using namespace std;
 const int maxn=1e6+8;
 struct data{
@@ -6,6 +10,10 @@ struct data{
 }e[maxn<<1];
 int n,cnt,m,id;
 int last[maxn],q[maxn],st[maxn],ed[maxn];
+int rl[maxn<<2],rr[maxn<<2],sum[maxn<<2];
+//vector<int> E[maxn];
+typedef vector<int> ve;
+vector<ve> E(maxn);
 void init(){
 	cnt=id=0;
 	memset(last,0,sizeof(last));
@@ -18,23 +26,50 @@ void ins(int u,int v)
 	e[++cnt].to=u;e[cnt].next=last[v];last[v]=cnt;
 }
 
-void dfs(int x,int fa)
+void dfs(int x)
 {
-    cout<<"begin"<<endl;
-    cout<<x<<' '<<fa<<endl;
     q[++id]=x;
     st[x]=id;
-    for(int i=last[x];i;i=e[i].next)
+    for(int i=0;i<E[x].size();i++)
     {
-        if(e[i].to!=fa)
-        {
-            dfs(e[i].to,x);
-        }
+        dfs(E[x][i]);
     }
     ed[x]=id;
-    cout<<x<<' '<<id<<endl;
+}
+void build(int k,int l,int r)
+{
+    int mid=(l+r)>>1;
+    rl[k]=l;rr[k]=r;
+    if(l==r)
+    {
+        sum[k]=1;
+        return ;
+    }
+    build(k<<1,l,mid);
+    build(k<<1|1,mid+1,r);
+    sum[k]=sum[k<<1]+sum[k<<1|1];
+}
+int query(int k,int a,int b)
+{
+    int l=rl[k],r=rr[k],mid=(l+r)>>1;
+    if(l==a&&r==b) return sum[k];
+    if(b<=mid) return query(k<<1,a,b);
+    else if(a>mid) return query(k<<1|1,a,b);
+    else return query(k<<1,a,mid)+query(k<<1|1,mid+1,b);
 }
 
+void modify(int k,int x)
+{
+    int l=rl[k],r=rr[k],mid=(l+r)>>1;
+    if(l==r)
+    {
+        sum[k]^=1;
+        return;
+    }
+    if(x<=mid) modify(k<<1,x);
+    else modify(k<<1|1,x);
+    sum[k]=sum[k<<1]+sum[k<<1|1];
+}
 int main()
 {
 	scanf("%d",&n);
@@ -42,11 +77,21 @@ int main()
 	for(int i =1;i<n;i++)
 	{
 		int u,v;scanf("%d%d",&u,&v);
-		ins(u,v);
+		//ins(u,v);
+		E[u].push_back(v);
 	}
-	dfs(1,0);
+	dfs(1);
+	build(1,1,n);
 	scanf("%d",&m);
-	char ch[10];
-	//build(1,1,n);
+	char ch;
+
+	while(m--)
+    {
+        int x;
+        scanf("%*c%c%d",&ch,&x);
+        if(ch=='Q')
+            printf("%d\n",query(1,st[x],ed[x]));
+        else modify(1,st[x]);
+    }
 	return 0;
 }
